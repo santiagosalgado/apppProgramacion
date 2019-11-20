41788cbeef896c5c2452f39ecf5ff43caed9dede
santiago <santiago.salgado@pucese.edu.ec>
2019-11-19 20:44:03 -0500

    formula general tarea  :3

---
+++ apptareaclase/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace apptareaclase
+{
+    public static class tareaformula
+    {
+        public static double x1(double a, double b, double c)
+        {
+            double x1 = 0, d = 0;
+                d = (b * b) - (4 * a * c);
+            x1 = (-b - Math.Sqrt(d)) / (2 * a);
+            return x1;
+        }
+        public static double x2(double a, double b, double c)
+        {
+            double x2 = 0, d = 0;
+            d = (b * b) - (4 * a * c);
+            x2 = (-b - Math.Sqrt(d)) / (2 * a);
+            return x2;
+        }
+        
+    }
+}
